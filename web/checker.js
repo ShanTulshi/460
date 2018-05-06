3f5d98d2943a8bb413a8e64ab50ad08cec9a571c
Shan Tulshi <shan.tulshi@gmail.com>
2018-05-06 15:45:01 -0500

    solution 0

--- web/checker.js
+++ web/checker.js
@@ -2,20 +2,24 @@ let express = require('express'),
 		router = express.Router()
 	;
 
-const checkExistence = (() => {
-	var solutions = require('./sols.js')	// Keep sols.js private
-	return (sol, challenge) => {solutions[challenge] === sol};
-})();
+const checkExistence = ((challenge, solfile = './sols.js') => {
+	let solutions = require(solfile)	// Keep sols.js private
+	return (sol) => {
+		return solutions[challenge] === sol
+	};
+});
 
 const checker = {
-	0: checkExistence,
+	0: checkExistence(0),
 };
 
 router.get('/:num', (req, res) => {
-	if(checker[req.params.num])
-		res.send(checker[req.params.num](req.query.solution));
-	else
-		return
+	if(checker[req.params.num]) {
+		res.status(202)
+			.json({result: checker[req.params.num](req.query.attempt)})
+			.send();
+	} else
+		res.sendStatus(403);
 });
 
 module.exports = router;
