b4c8b6c26189bcb10c76e39c583428d1f9ff650e
Shan Tulshi <shan.tulshi@gmail.com>
2018-05-06 14:42:05 -0500

    add sols.js for privacy

---
+++ web/checker.js
@@ -0,0 +1,21 @@
+let express = require('express'),
+		router = express.Router()
+	;
+
+const checkExistence = (() => {
+	var solutions = require('./sols.js')	// Keep sols.js private
+	return (sol, challenge) => {solutions[challenge] === sol};
+})();
+
+const checker = {
+	0: checkExistence,
+};
+
+router.get('/:num', (req, res) => {
+	if(checker[req.params.num])
+		res.send(checker[req.params.num](req.query.solution));
+	else
+		return
+});
+
+module.exports = router;
